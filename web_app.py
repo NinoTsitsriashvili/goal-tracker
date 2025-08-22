9685ac2f25988b5d55f5139131989ae58d638ce9
NinoTsitsriashvili <wiwri3@gmail.com>
2025-08-22 19:31:09 +0400

    Your message2333

--- web_app.py
+++ web_app.py
@@ -20,7 +20,7 @@ st.markdown("""
     }
     .main-header {
         text-align: center;
-        color: #6B8E6B;
+        color: #8BA68B;
         font-size: 3rem;
         margin-bottom: 2rem;
     }
